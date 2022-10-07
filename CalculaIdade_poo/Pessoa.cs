c21a34b05c0fc44db4e0e9a33d50a4582a081749
Daniel Gongo <dan_liveira@outlook.com>
2022-10-07 09:41:45 -0300

    Projeto simples C# calcular idade

---
+++ CalculaIdade_poo/Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaIdade_poo
+{
+    public class Pessoa
+    {
+        private int anoNascimento;
+        private string nome;
+
+        public int AnoNascimento
+        {
+            get { return this.anoNascimento; }
+
+            set
+            {
+                this.anoNascimento = value;
+            }
+        }
+
+        public string Nome
+        {
+            get { return this.nome; }
+            set
+            {
+                this.nome = value.ToUpper(); //ToUpper para deixar as letras maiusculas
+            }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Ano de mascimento: " + this.AnoNascimento);
+            Console.WriteLine("Idade: " + this.CalcularIdade() + " anos");
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime data = DateTime.Now;
+            int ano =  data.Year;
+
+            int idade = ano - AnoNascimento;
+
+            return idade;
+        }
+    }
+}
---
+++ CalculaIdade_poo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaIdade_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MINI PROGRAMA CALCULAR IDADE");
+
+            Pessoa people = new Pessoa();
+
+            Console.Write("Nome da Pessoa: ");
+            people.Nome = Console.ReadLine();
+
+            Console.Write("Ano de nascimento: ");
+            people.AnoNascimento = int.Parse(Console.ReadLine());
+
+            people.ExibirDados();
+
+            Console.ReadKey();
+        }
+    }
+}
